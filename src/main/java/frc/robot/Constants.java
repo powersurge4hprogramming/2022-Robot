ced007cf040e0cbd037b78d56f65464931c60f1b
powersurge4hprogramming <58537304+powersurge4hprogramming@users.noreply.github.com>
2022-02-09 21:07:00 -0500

    Improved scale
    good good very good no bad good
    Scale and slew
    good

--- src/main/java/frc/robot/Constants.java
+++ src/main/java/frc/robot/Constants.java
@@ -5,11 +5,15 @@
 package frc.robot;
 
 /**
- * The Constants class provides a convenient place for teams to hold robot-wide numerical or boolean
- * constants. This class should not be used for any other purpose. All constants should be declared
+ * The Constants class provides a convenient place for teams to hold robot-wide
+ * numerical or boolean
+ * constants. This class should not be used for any other purpose. All constants
+ * should be declared
  * globally (i.e. public static). Do not put anything functional in this class.
  *
- * <p>It is advised to statically import this class (or one of its inner classes) wherever the
+ * <p>
+ * It is advised to statically import this class (or one of its inner classes)
+ * wherever the
  * constants are needed, to reduce verbosity.
  */
 public final class Constants {
@@ -18,11 +22,14 @@ public final class Constants {
     public static final int DRIVER_JOYSTICK_Y_AXIS = 1;
     public static final int DRIVER_JOYSTICK_Z_AXIS = 2;
     public static final int DRIVER_JOYSTICK_SCALE_AXIS = 3;
-    public static final int DRIVER_JOYSTICK_PORT = 0;
 
+    public static final int DRIVER_JOYSTICK_PORT = 0;
+    public static final double DRIVER_LATERAL_SLEW = 5;
+    public static final double DRIVER_TWIST_SLEW = 5;
 
     public static final int FRONT_LEFT_MOTOR_CONTROL = 0;
     public static final int FRONT_RIGHT_MOTOR_CONTROL = 1;
     public static final int BACK_LEFT_MOTOR_CONTROL = 2;
     public static final int BACK_RIGHT_MOTOR_CONTROL = 3;
+
 }
--- src/main/java/frc/robot/commands/DriveCommand.java
+++ src/main/java/frc/robot/commands/DriveCommand.java
@@ -4,36 +4,53 @@
 
 package frc.robot.commands;
 
+import edu.wpi.first.math.filter.SlewRateLimiter;
 import edu.wpi.first.wpilibj.GenericHID;
 import edu.wpi.first.wpilibj2.command.CommandBase;
 import frc.robot.Constants;
 import frc.robot.subsystems.Drivetrain;
+import frc.robot.utilities.MathU;
 
 public class DriveCommand extends CommandBase {
 
   private final Drivetrain drivetrain;
   private final GenericHID driveJoystick;
+  private final SlewRateLimiter yLimiter;
+  private final SlewRateLimiter xLimiter;
+  private final SlewRateLimiter zLimiter;
 
   /** Creates a new DriveCommand. */
   public DriveCommand(Drivetrain drivetrain, GenericHID driveJoystick) {
-      this.drivetrain= drivetrain;
-      this.driveJoystick = driveJoystick;
-      addRequirements(drivetrain);
-        // Use addRequirements() here to declare subsystem dependencies.
+    this.drivetrain = drivetrain;
+    this.driveJoystick = driveJoystick;
+    yLimiter = new SlewRateLimiter(Constants.DRIVER_LATERAL_SLEW);
+    xLimiter = new SlewRateLimiter(Constants.DRIVER_LATERAL_SLEW);
+    zLimiter = new SlewRateLimiter(Constants.DRIVER_TWIST_SLEW);
+    addRequirements(drivetrain);
+    // Use addRequirements() here to declare subsystem dependencies.
   }
 
   // Called when the command is initially scheduled.
   @Override
-  public void initialize() {}
+  public void initialize() {
+  }
 
   // Called every time the scheduler runs while the command is scheduled.
   @Override
   public void execute() {
-      Float Xaxis= (float) driveJoystick.getRawAxis(Constants.DRIVER_JOYSTICK_X_AXIS);
-      Float Yaxis= (float) -driveJoystick.getRawAxis(Constants.DRIVER_JOYSTICK_Y_AXIS);
-      Float Zaxis= (float) driveJoystick.getRawAxis(Constants.DRIVER_JOYSTICK_Z_AXIS);
-//                       y      x      z
-      drivetrain.drive(Yaxis, Xaxis, Zaxis);
+    double xAxis = driveJoystick.getRawAxis(Constants.DRIVER_JOYSTICK_X_AXIS);
+    double yAxis = -driveJoystick.getRawAxis(Constants.DRIVER_JOYSTICK_Y_AXIS);
+    double zAxis = driveJoystick.getRawAxis(Constants.DRIVER_JOYSTICK_Z_AXIS);
+
+    double scale = driveJoystick.getRawAxis(Constants.DRIVER_JOYSTICK_SCALE_AXIS);
+    scale = (1 - scale) / 2;
+
+    xAxis = xLimiter.calculate(MathU.squareInput(xAxis) * scale);
+    yAxis = yLimiter.calculate(MathU.squareInput(yAxis) * scale);
+    zAxis = zLimiter.calculate(MathU.squareInput(zAxis) * scale);
+
+    // y x z
+    drivetrain.drive((float) yAxis, (float) xAxis, (float) zAxis);
   }
 
   // Called once the command ends or is interrupted.
