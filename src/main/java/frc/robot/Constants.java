f9179dc49ec8f7b3718b7a5def603537c1e9a934
powersurge4hprogramming <58537304+powersurge4hprogramming@users.noreply.github.com>
2022-02-12 17:01:48 -0500

    init auto
    with pose in subsystem and robotContainer

--- src/main/java/frc/robot/Constants.java
+++ src/main/java/frc/robot/Constants.java
@@ -4,6 +4,11 @@
 
 package frc.robot;
 
+import edu.wpi.first.math.controller.SimpleMotorFeedforward;
+import edu.wpi.first.math.geometry.Translation2d;
+import edu.wpi.first.math.kinematics.MecanumDriveKinematics;
+import edu.wpi.first.math.trajectory.TrapezoidProfile;
+
 /**
  * The Constants class provides a convenient place for teams to hold robot-wide
  * numerical or boolean
@@ -17,6 +22,49 @@ package frc.robot;
  * constants are needed, to reduce verbosity.
  */
 public final class Constants {
+    public static final class DriveConstants {
+
+        public static final int FRONT_LEFT_MOTOR_CONTROL = 0;
+        public static final int FRONT_RIGHT_MOTOR_CONTROL = 1;
+        public static final int BACK_LEFT_MOTOR_CONTROL = 2;
+        public static final int BACK_RIGHT_MOTOR_CONTROL = 3;
+
+        public static final boolean kFrontLeftEncoderReversed = false;
+        public static final boolean kRearLeftEncoderReversed = true;
+        public static final boolean kFrontRightEncoderReversed = false;
+        public static final boolean kRearRightEncoderReversed = true;
+
+        public static final double kTrackWidth = 0.5;
+        // Distance between centers of right and left wheels on robot
+        public static final double kWheelBase = 0.7;
+        // Distance between centers of front and back wheels on robot
+
+        public static final MecanumDriveKinematics kDriveKinematics = new MecanumDriveKinematics(
+                new Translation2d(kWheelBase / 2, kTrackWidth / 2),
+                new Translation2d(kWheelBase / 2, -kTrackWidth / 2),
+                new Translation2d(-kWheelBase / 2, kTrackWidth / 2),
+                new Translation2d(-kWheelBase / 2, -kTrackWidth / 2));
+
+        public static final int kEncoderCPR = 1024;
+        public static final double kWheelDiameterMeters = 0.15;
+        public static final double kEncoderDistancePerPulse =
+                // Assumes the encoders are directly mounted on the wheel shafts
+                (kWheelDiameterMeters * Math.PI) / (double) kEncoderCPR;
+
+        // These are example values only - DO NOT USE THESE FOR YOUR OWN ROBOT!
+        // These characterization values MUST be determined either experimentally or
+        // theoretically
+        // for *your* robot's drive.
+        // The SysId tool provides a convenient method for obtaining these values for
+        // your robot.
+        public static final SimpleMotorFeedforward kFeedforward = new SimpleMotorFeedforward(1, 0.8, 0.15);
+
+        // Example value only - as above, this must be tuned for your drive!
+        public static final double kPFrontLeftVel = 0.5;
+        public static final double kPRearLeftVel = 0.5;
+        public static final double kPFrontRightVel = 0.5;
+        public static final double kPRearRightVel = 0.5;
+    }
 
     public static final int DRIVER_JOYSTICK_X_AXIS = 0;
     public static final int DRIVER_JOYSTICK_Y_AXIS = 1;
@@ -28,11 +76,6 @@ public final class Constants {
     public static final double DRIVER_LATERAL_SLEW = 5;
     public static final double DRIVER_TWIST_SLEW = 5;
 
-    public static final int FRONT_LEFT_MOTOR_CONTROL = 0;
-    public static final int FRONT_RIGHT_MOTOR_CONTROL = 1;
-    public static final int BACK_LEFT_MOTOR_CONTROL = 2;
-    public static final int BACK_RIGHT_MOTOR_CONTROL = 3;
-
     public static final int intake = 4;
 
     public static final int Shooterport = 5;
@@ -41,4 +84,20 @@ public final class Constants {
     public static final int Intakebutton = 3;
 
     public static final int Shooteraxis = 3;
+
+    public final static class AutoConstants {
+        public static final double kMaxSpeedMetersPerSecond = 3;
+        public static final double kMaxAccelerationMetersPerSecondSquared = 3;
+        public static final double kMaxAngularSpeedRadiansPerSecond = Math.PI;
+        public static final double kMaxAngularSpeedRadiansPerSecondSquared = Math.PI;
+
+        public static final double kPXController = 0.5;
+        public static final double kPYController = 0.5;
+        public static final double kPThetaController = 0.5;
+
+        // Constraint for the motion profilied robot angle controller
+        public static final TrapezoidProfile.Constraints kThetaControllerConstraints = new TrapezoidProfile.Constraints(
+                kMaxAngularSpeedRadiansPerSecond, kMaxAngularSpeedRadiansPerSecondSquared);
+    }
+
 }
--- src/main/java/frc/robot/RobotContainer.java
+++ src/main/java/frc/robot/RobotContainer.java
@@ -4,9 +4,20 @@
 
 package frc.robot;
 
+import java.util.List;
+
+import edu.wpi.first.math.controller.PIDController;
+import edu.wpi.first.math.controller.ProfiledPIDController;
+import edu.wpi.first.math.geometry.Pose2d;
+import edu.wpi.first.math.geometry.Rotation2d;
+import edu.wpi.first.math.geometry.Translation2d;
+import edu.wpi.first.math.trajectory.Trajectory;
+import edu.wpi.first.math.trajectory.TrajectoryConfig;
+import edu.wpi.first.math.trajectory.TrajectoryGenerator;
 import edu.wpi.first.wpilibj.GenericHID;
-import edu.wpi.first.wpilibj.Joystick;
 import edu.wpi.first.wpilibj.XboxController;
+import frc.robot.Constants.AutoConstants;
+import frc.robot.Constants.DriveConstants;
 import frc.robot.commands.DriveCommand;
 import frc.robot.commands.Intakecommand;
 import frc.robot.commands.Shootercommand;
@@ -14,19 +25,25 @@ import frc.robot.subsystems.Drivetrain;
 import frc.robot.subsystems.Intake;
 import frc.robot.subsystems.Shooter;
 import edu.wpi.first.wpilibj2.command.Command;
+import edu.wpi.first.wpilibj2.command.MecanumControllerCommand;
 import edu.wpi.first.wpilibj2.command.button.JoystickButton;
 
 /**
- * This class is where the bulk of the robot should be declared. Since Command-based is a
- * "declarative" paradigm, very little robot logic should actually be handled in the {@link Robot}
- * periodic methods (other than the scheduler calls). Instead, the structure of the robot (including
+ * This class is where the bulk of the robot should be declared. Since
+ * Command-based is a
+ * "declarative" paradigm, very little robot logic should actually be handled in
+ * the {@link Robot}
+ * periodic methods (other than the scheduler calls). Instead, the structure of
+ * the robot (including
  * subsystems, commands, and button mappings) should be declared here.
  */
 public class RobotContainer {
   // The robot's subsystems and commands are defined here...
   private final GenericHID m_driveJoystick = new GenericHID(Constants.DRIVER_JOYSTICK_PORT);
   private final GenericHID m_operatorJoystick = new GenericHID(Constants.OPERATOR_JOYSTICK_PORT);
-  private final Drivetrain m_drivetrain = new Drivetrain(Constants.FRONT_LEFT_MOTOR_CONTROL, Constants.FRONT_RIGHT_MOTOR_CONTROL, Constants.BACK_LEFT_MOTOR_CONTROL, Constants.BACK_RIGHT_MOTOR_CONTROL);
+  private final Drivetrain m_drivetrain = new Drivetrain(DriveConstants.FRONT_LEFT_MOTOR_CONTROL,
+      DriveConstants.FRONT_RIGHT_MOTOR_CONTROL, DriveConstants.BACK_LEFT_MOTOR_CONTROL,
+      DriveConstants.BACK_RIGHT_MOTOR_CONTROL);
   private final Shooter m_shooter = new Shooter(Constants.Shooterport, Constants.servoport);
 
   private final Intake m_Intake = new Intake(Constants.intake);
@@ -34,32 +51,82 @@ public class RobotContainer {
   private final DriveCommand m_teleopCommand = new DriveCommand(m_drivetrain, m_driveJoystick);
   private final Shootercommand m_shooterCommand = new Shootercommand(m_shooter, m_operatorJoystick);
 
-  /** The container for the robot. Contains subsystems, OI devices, and commands. */
+  /**
+   * The container for the robot. Contains subsystems, OI devices, and commands.
+   */
   public RobotContainer() {
     // Configure the button bindings
     configureButtonBindings();
   }
-  
-  public void teleopStart(){
+
+  public void teleopStart() {
     m_drivetrain.setDefaultCommand(m_teleopCommand);
     m_shooter.setDefaultCommand(m_shooterCommand);
   }
+
   /**
-   * Use this method to define your button->command mappings. Buttons can be created by
+   * Use this method to define your button->command mappings. Buttons can be
+   * created by
    * instantiating a {@link GenericHID} or one of its subclasses ({@link
-   * edu.wpi.first.wpilibj.Joystick} or {@link XboxController}), and then passing it to a {@link
+   * edu.wpi.first.wpilibj.Joystick} or {@link XboxController}), and then passing
+   * it to a {@link
    * edu.wpi.first.wpilibj2.command.button.JoystickButton}.
    */
   private void configureButtonBindings() {
-      new JoystickButton(m_operatorJoystick, Constants.Intakebutton).whenHeld(new Intakecommand(m_Intake));
+    new JoystickButton(m_operatorJoystick, Constants.Intakebutton).whenHeld(new Intakecommand(m_Intake));
   }
 
   /**
    * Use this to pass the autonomous command to the main {@link Robot} class.
+   * 
    * @return the command to run in autonomous
    */
   public Command getAutonomousCommand() {
-    // An ExampleCommand will run in autonomous
-    return null;
+    // Create config for trajectory
+    TrajectoryConfig config = new TrajectoryConfig(
+        AutoConstants.kMaxSpeedMetersPerSecond,
+        AutoConstants.kMaxAccelerationMetersPerSecondSquared)
+            // Add kinematics to ensure max speed is actually obeyed
+            .setKinematics(DriveConstants.kDriveKinematics);
+
+    // An example trajectory to follow. All units in meters.
+    Trajectory exampleTrajectory = TrajectoryGenerator.generateTrajectory(
+        // Start at the origin facing the +X direction
+        new Pose2d(0, 0, new Rotation2d(0)),
+        // Pass through these two interior waypoints, making an 's' curve path
+        List.of(new Translation2d(1, 1), new Translation2d(2, -1)),
+        // End 3 meters straight ahead of where we started, facing forward
+        new Pose2d(3, 0, new Rotation2d(0)),
+        config);
+
+    MecanumControllerCommand mecanumControllerCommand = new MecanumControllerCommand(
+        exampleTrajectory,
+        m_drivetrain::getPose,
+        DriveConstants.kFeedforward,
+        DriveConstants.kDriveKinematics,
+
+        // Position contollers
+        new PIDController(AutoConstants.kPXController, 0, 0),
+        new PIDController(AutoConstants.kPYController, 0, 0),
+        new ProfiledPIDController(
+            AutoConstants.kPThetaController, 0, 0, AutoConstants.kThetaControllerConstraints),
+
+        // Needed for normalizing wheel speeds
+        AutoConstants.kMaxSpeedMetersPerSecond,
+
+        // Velocity PID's
+        new PIDController(DriveConstants.kPFrontLeftVel, 0, 0),
+        new PIDController(DriveConstants.kPRearLeftVel, 0, 0),
+        new PIDController(DriveConstants.kPFrontRightVel, 0, 0),
+        new PIDController(DriveConstants.kPRearRightVel, 0, 0),
+        m_drivetrain::getCurrentWheelSpeeds,
+        m_drivetrain::setDriveMotorControllersVolts, // Consumer for the output motor voltages
+        m_drivetrain);
+
+    // Reset odometry to the starting pose of the trajectory.
+    m_drivetrain.resetOdometry(exampleTrajectory.getInitialPose());
+
+    // Run path following command, then stop at the end.
+    return mecanumControllerCommand.andThen(() -> m_drivetrain.drive(0, 0, 0, false));
   }
 }
