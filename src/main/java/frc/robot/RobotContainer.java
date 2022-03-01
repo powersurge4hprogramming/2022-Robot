ee0cc3fc26dcd926b1df6a8b9fd3c391eb39db91
powersurge4hprogramming <58537304+powersurge4hprogramming@users.noreply.github.com>
2022-02-28 21:15:01 -0500

    add initial auto
    no distances yet, prob bad implementation

--- src/main/java/frc/robot/RobotContainer.java
+++ src/main/java/frc/robot/RobotContainer.java
@@ -32,6 +32,8 @@ import frc.robot.subsystems.Shooter;
 import edu.wpi.first.wpilibj2.command.Command;
 import edu.wpi.first.wpilibj2.command.MecanumControllerCommand;
 import edu.wpi.first.wpilibj2.command.ParallelCommandGroup;
+import edu.wpi.first.wpilibj2.command.ParallelRaceGroup;
+import edu.wpi.first.wpilibj2.command.SequentialCommandGroup;
 import edu.wpi.first.wpilibj2.command.button.JoystickButton;
 
 /**
@@ -44,118 +46,164 @@ import edu.wpi.first.wpilibj2.command.button.JoystickButton;
  * subsystems, commands, and button mappings) should be declared here.
  */
 public class RobotContainer {
-    // Input Devices
-    private final GenericHID m_driveJoystick = new GenericHID(Constants.InputConstants.DRIVER_JOYSTICK_PORT);
-    private final GenericHID m_operatorJoystick = new GenericHID(Constants.InputConstants.OPERATOR_JOYSTICK_PORT);
+        // Input Devices
+        private final GenericHID m_driveJoystick = new GenericHID(Constants.InputConstants.DRIVER_JOYSTICK_PORT);
+        private final GenericHID m_operatorJoystick = new GenericHID(Constants.InputConstants.OPERATOR_JOYSTICK_PORT);
 
-    // Subsystems
-    private final Drivetrain m_drivetrain = new Drivetrain(Constants.MotorConstants.FRONT_LEFT_MOTOR_CONTROL,
-            Constants.MotorConstants.FRONT_RIGHT_MOTOR_CONTROL, Constants.MotorConstants.BACK_LEFT_MOTOR_CONTROL,
-            Constants.MotorConstants.BACK_RIGHT_MOTOR_CONTROL);
-    private final Shooter m_shooter = new Shooter(Constants.MotorConstants.SHOOTER_PORT);
-    private final Intake m_Intake = new Intake(Constants.MotorConstants.INTAKE_PORT,
-            Constants.MotorConstants.TROUGH_PORT);
-    private final Gatekeeper m_gatekeeper = new Gatekeeper(Constants.MotorConstants.GATEKEEPER_PORT);
-    private final Climber m_climber = new Climber(Constants.MotorConstants.CLIMBER_PORT,
-            Constants.MotorConstants.RELEASE_MOTOR_PORT);
+        // Subsystems
+        private final Drivetrain m_drivetrain = new Drivetrain(Constants.MotorConstants.FRONT_LEFT_MOTOR_CONTROL,
+                        Constants.MotorConstants.FRONT_RIGHT_MOTOR_CONTROL,
+                        Constants.MotorConstants.BACK_LEFT_MOTOR_CONTROL,
+                        Constants.MotorConstants.BACK_RIGHT_MOTOR_CONTROL);
+        private final Shooter m_shooter = new Shooter(Constants.MotorConstants.SHOOTER_PORT);
+        private final Intake m_Intake = new Intake(Constants.MotorConstants.INTAKE_PORT,
+                        Constants.MotorConstants.TROUGH_PORT);
+        private final Gatekeeper m_gatekeeper = new Gatekeeper(Constants.MotorConstants.GATEKEEPER_PORT);
+        private final Climber m_climber = new Climber(Constants.MotorConstants.CLIMBER_PORT,
+                        Constants.MotorConstants.RELEASE_MOTOR_PORT);
 
-    // Commands
-    private final DriveCommand m_teleopCommand = new DriveCommand(m_drivetrain, m_driveJoystick);
-    private final DashboardShooterCommand m_shooterCommand = new DashboardShooterCommand(m_shooter);
-    private final LimeReader m_limeReader = new LimeReader();
+        // Commands
+        private final DriveCommand m_teleopCommand = new DriveCommand(m_drivetrain, m_driveJoystick);
+        private final DashboardShooterCommand m_shooterCommand = new DashboardShooterCommand(m_shooter);
+        private final LimeReader m_limeReader = new LimeReader();
 
-    /**
-     * The container for the robot. Contains subsystems, OI devices, and commands.
-     */
-    public RobotContainer() {
-        // Configure the button bindings
-        configureButtonBindings();
-    }
+        /**
+         * The container for the robot. Contains subsystems, OI devices, and commands.
+         */
+        public RobotContainer() {
+                // Configure the button bindings
+                configureButtonBindings();
+        }
 
-    public void teleopStart() {
-        m_limeReader.schedule();
-        m_drivetrain.setDefaultCommand(m_teleopCommand);
-        m_shooter.setDefaultCommand(m_shooterCommand);
-    }
+        public void teleopStart() {
+                m_limeReader.schedule();
+                m_drivetrain.setDefaultCommand(m_teleopCommand);
+                m_shooter.setDefaultCommand(m_shooterCommand);
+        }
 
-    /**
-     * Use this method to define your button->command mappings. Buttons can be
-     * created by
-     * instantiating a {@link GenericHID} or one of its subclasses ({@link
-     * edu.wpi.first.wpilibj.Joystick} or {@link XboxController}), and then passing
-     * it to a {@link
-     * edu.wpi.first.wpilibj2.command.button.JoystickButton}.
-     */
-    private void configureButtonBindings() {
-        new JoystickButton(m_operatorJoystick, Constants.InputConstants.INTAKE_BUTTON)
-                .whenHeld(new IntakeCommand(m_Intake));
+        /**
+         * Use this method to define your button->command mappings. Buttons can be
+         * created by
+         * instantiating a {@link GenericHID} or one of its subclasses ({@link
+         * edu.wpi.first.wpilibj.Joystick} or {@link XboxController}), and then passing
+         * it to a {@link
+         * edu.wpi.first.wpilibj2.command.button.JoystickButton}.
+         */
+        private void configureButtonBindings() {
+                new JoystickButton(m_operatorJoystick, Constants.InputConstants.INTAKE_BUTTON)
+                                .whenHeld(new IntakeCommand(m_Intake));
 
-        new JoystickButton(m_operatorJoystick, Constants.InputConstants.MECH_AIM_BUTTON)
-                .whenHeld(new ParallelCommandGroup(new VisionShooterCommand(m_shooter),
-                        new MechAimCommand(m_drivetrain)));
+                new JoystickButton(m_operatorJoystick, Constants.InputConstants.MECH_AIM_BUTTON)
+                                .whenHeld(new ParallelCommandGroup(new VisionShooterCommand(m_shooter),
+                                                new MechAimCommand(m_drivetrain)));
 
-        new JoystickButton(m_operatorJoystick, Constants.InputConstants.GATEKEEPER_ALLOW_BUTTON)
-                .whenPressed(
-                        new GatekeeperCommand(m_gatekeeper)
-                                .withTimeout(Constants.BehaviorConstants.GATEKEEPER_ALLOW_TIME));
+                new JoystickButton(m_operatorJoystick, Constants.InputConstants.GATEKEEPER_ALLOW_BUTTON)
+                                .whenPressed(
+                                                new GatekeeperCommand(m_gatekeeper)
+                                                                .withTimeout(Constants.BehaviorConstants.GATEKEEPER_ALLOW_TIME));
 
-        new JoystickButton(m_operatorJoystick, Constants.InputConstants.CLIMB_BUTTON)
-                .whenHeld(new ClimbCommand(m_climber));
+                new JoystickButton(m_operatorJoystick, Constants.InputConstants.CLIMB_BUTTON)
+                                .whenHeld(new ClimbCommand(m_climber));
 
-    }
+        }
 
-    /**
-     * Use this to pass the autonomous command to the main {@link Robot} class.
-     * 
-     * @return the command to run in autonomous
-     */
-    public Command getAutonomousCommand() {
-        // Create config for trajectory
-        TrajectoryConfig config = new TrajectoryConfig(
-                Constants.DriveConstants.MAX_SPEED_M_PER_SEC,
-                Constants.DriveConstants.MAX_ACCEL_M_PER_SEC_SQUARED)
-                        // Add kinematics to ensure max speed is actually obeyed
-                        .setKinematics(Constants.DriveConstants.DRIVE_KINEMATICS);
+        /**
+         * Use this to pass the autonomous command to the main {@link Robot} class.
+         * 
+         * @return the command to run in autonomous
+         */
+        public Command getAutonomousCommand() {
+                // Create config for trajectory
+                TrajectoryConfig config = new TrajectoryConfig(
+                                Constants.DriveConstants.MAX_SPEED_M_PER_SEC,
+                                Constants.DriveConstants.MAX_ACCEL_M_PER_SEC_SQUARED)
+                                                // Add kinematics to ensure max speed is actually obeyed
+                                                .setKinematics(Constants.DriveConstants.DRIVE_KINEMATICS);
 
-        // An example trajectory to follow. All units in meters.
-        Trajectory exampleTrajectory = TrajectoryGenerator.generateTrajectory(
-                // Start at the origin facing the +X direction
-                new Pose2d(0, 0, new Rotation2d(0)),
-                // Pass through these two interior waypoints, making an 's' curve path
-                List.of(new Translation2d(1, 1), new Translation2d(2, -1)),
-                // End 3 meters straight ahead of where we started, facing forward
-                new Pose2d(3, 0, new Rotation2d(0)),
-                config);
+                // An example trajectory to follow. All units in meters.
+                Trajectory mechTrajectory1 = TrajectoryGenerator.generateTrajectory(
+                                // Start at the origin facing the +X direction
+                                new Pose2d(0, 0, new Rotation2d(0)),
+                                // Pass through these two interior waypoints, making an 's' curve path
+                                List.of(new Translation2d(1, 1), new Translation2d(2, -1)),
+                                // End 3 meters straight ahead of where we started, facing forward
+                                new Pose2d(3, 0, new Rotation2d(0)),
+                                config);
 
-        MecanumControllerCommand mecanumControllerCommand = new MecanumControllerCommand(
-                exampleTrajectory,
-                m_drivetrain::getPose,
-                Constants.DriveConstants.MOTOR_FEED_FORWARD,
-                Constants.DriveConstants.DRIVE_KINEMATICS,
+                Trajectory mechTrajectory2 = TrajectoryGenerator.generateTrajectory(
+                                // Start at the origin facing the +X direction
+                                new Pose2d(0, 0, new Rotation2d(0)),
+                                // Pass through these two interior waypoints, making an 's' curve path
+                                List.of(new Translation2d(1, 1), new Translation2d(2, -1)),
+                                // End 3 meters straight ahead of where we started, facing forward
+                                new Pose2d(3, 0, new Rotation2d(0)),
+                                config);
 
-                // Position contollers
-                new PIDController(Constants.DriveConstants.PX_CONTROLLER, 0, 0),
-                new PIDController(Constants.DriveConstants.PY_CONTROLLER, 0, 0),
-                new ProfiledPIDController(
-                        Constants.DriveConstants.P_THETA_CONTROLLER, 0, 0,
-                        Constants.DriveConstants.THETA_CONTROLLER_CONSTRAINTS),
+                MecanumControllerCommand mechMove1 = new MecanumControllerCommand(
+                                mechTrajectory1,
+                                m_drivetrain::getPose,
+                                Constants.DriveConstants.MOTOR_FEED_FORWARD,
+                                Constants.DriveConstants.DRIVE_KINEMATICS,
 
-                // Needed for normalizing wheel speeds
-                Constants.DriveConstants.MAX_SPEED_M_PER_SEC,
+                                // Position contollers
+                                new PIDController(Constants.DriveConstants.PX_CONTROLLER, 0, 0),
+                                new PIDController(Constants.DriveConstants.PY_CONTROLLER, 0, 0),
+                                new ProfiledPIDController(
+                                                Constants.DriveConstants.P_THETA_CONTROLLER, 0, 0,
+                                                Constants.DriveConstants.THETA_CONTROLLER_CONSTRAINTS),
 
-                // Velocity PID's
-                new PIDController(Constants.DriveConstants.FL_VELOCITY, 0, 0),
-                new PIDController(Constants.DriveConstants.RL_VELOCITY, 0, 0),
-                new PIDController(Constants.DriveConstants.FR_VELOCITY, 0, 0),
-                new PIDController(Constants.DriveConstants.RR_VELOCITY, 0, 0),
-                m_drivetrain::getCurrentWheelSpeeds,
-                m_drivetrain::setDriveMotorControllersVolts, // Consumer for the output motor voltages
-                m_drivetrain);
+                                // Needed for normalizing wheel speeds
+                                Constants.DriveConstants.MAX_SPEED_M_PER_SEC,
 
-        // Reset odometry to the starting pose of the trajectory.
-        m_drivetrain.resetOdometry(exampleTrajectory.getInitialPose());
+                                // Velocity PID's
+                                new PIDController(Constants.DriveConstants.FL_VELOCITY, 0, 0),
+                                new PIDController(Constants.DriveConstants.RL_VELOCITY, 0, 0),
+                                new PIDController(Constants.DriveConstants.FR_VELOCITY, 0, 0),
+                                new PIDController(Constants.DriveConstants.RR_VELOCITY, 0, 0),
+                                m_drivetrain::getCurrentWheelSpeeds,
+                                m_drivetrain::setDriveMotorControllersVolts, // Consumer for the output motor voltages
+                                m_drivetrain);
 
-        // Run path following command, then stop at the end.
-        return mecanumControllerCommand.andThen(() -> m_drivetrain.drive(0, 0, 0, false));
-    }
+                MecanumControllerCommand mechMove2 = new MecanumControllerCommand(
+                                mechTrajectory2,
+                                m_drivetrain::getPose,
+                                Constants.DriveConstants.MOTOR_FEED_FORWARD,
+                                Constants.DriveConstants.DRIVE_KINEMATICS,
+
+                                // Position contollers
+                                new PIDController(Constants.DriveConstants.PX_CONTROLLER, 0, 0),
+                                new PIDController(Constants.DriveConstants.PY_CONTROLLER, 0, 0),
+                                new ProfiledPIDController(
+                                                Constants.DriveConstants.P_THETA_CONTROLLER, 0, 0,
+                                                Constants.DriveConstants.THETA_CONTROLLER_CONSTRAINTS),
+
+                                // Needed for normalizing wheel speeds
+                                Constants.DriveConstants.MAX_SPEED_M_PER_SEC,
+
+                                // Velocity PID's
+                                new PIDController(Constants.DriveConstants.FL_VELOCITY, 0, 0),
+                                new PIDController(Constants.DriveConstants.RL_VELOCITY, 0, 0),
+                                new PIDController(Constants.DriveConstants.FR_VELOCITY, 0, 0),
+                                new PIDController(Constants.DriveConstants.RR_VELOCITY, 0, 0),
+                                m_drivetrain::getCurrentWheelSpeeds,
+                                m_drivetrain::setDriveMotorControllersVolts, // Consumer for the output motor voltages
+                                m_drivetrain);
+
+                // Reset odometry to the starting pose of the trajectory.
+                m_drivetrain.resetOdometry(mechTrajectory1.getInitialPose());
+
+                SequentialCommandGroup auto1 = new SequentialCommandGroup(
+                                new ParallelRaceGroup(new IntakeCommand(m_Intake),
+                                                mechMove1.andThen(() -> m_drivetrain.drive(0, 0, 0, false))),
+                                new ParallelRaceGroup(new MechAimCommand(m_drivetrain)
+                                                .andThen(new GatekeeperCommand(m_gatekeeper).withTimeout(
+                                                                Constants.BehaviorConstants.GATEKEEPER_ALLOW_TIME)),
+                                                new VisionShooterCommand(m_shooter)),
+                                new ParallelRaceGroup(new IntakeCommand(m_Intake), mechMove2
+                                                .andThen(() -> m_drivetrain.drive(0, 0, 0, false)))
+
+                );
+                // Run path following command, then stop at the end.
+                return auto1;
+        }
 }
