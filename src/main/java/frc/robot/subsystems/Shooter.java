bc14ea9252c8a03876878def2cf514614931cb16
powersurge4hprogramming <58537304+powersurge4hprogramming@users.noreply.github.com>
2022-03-14 21:04:20 -0400

    add talonfx gains

--- src/main/java/frc/robot/subsystems/Shooter.java
+++ src/main/java/frc/robot/subsystems/Shooter.java
@@ -5,32 +5,47 @@
 package frc.robot.subsystems;
 
 import com.ctre.phoenix.motorcontrol.ControlMode;
-import com.ctre.phoenix.motorcontrol.FeedbackDevice;
 import com.ctre.phoenix.motorcontrol.NeutralMode;
-import com.ctre.phoenix.motorcontrol.can.TalonFXConfiguration;
+import com.ctre.phoenix.motorcontrol.TalonFXFeedbackDevice;
 import com.ctre.phoenix.motorcontrol.can.WPI_TalonFX;
 
 import edu.wpi.first.wpilibj.smartdashboard.SmartDashboard;
 import edu.wpi.first.wpilibj2.command.SubsystemBase;
+
+import frc.robot.utilities.Gains;
 
 public class Shooter extends SubsystemBase {
   /** Creates a new Shooter. */
   private final WPI_TalonFX shooterMotor;
 
   public Shooter(int shooterPort) {
+    int timeout = 30;
+    Gains kGains_Velocit = new Gains(0.10, 0.001, 5, 1023.0 / 21940.0, 300, 1.00);
     shooterMotor = new WPI_TalonFX(shooterPort);
     shooterMotor.setInverted(true);
-
-    TalonFXConfiguration configs = new TalonFXConfiguration();
-    /* select integ-sensor for PID0 (it doesn't matter if PID is actually used) */
-    configs.primaryPID.selectedFeedbackSensor = FeedbackDevice.IntegratedSensor;
     /* config all the settings */
-    shooterMotor.configAllSettings(configs);
+    shooterMotor.configFactoryDefault();
+    shooterMotor.configNeutralDeadband(0.001);
     shooterMotor.setNeutralMode(NeutralMode.Coast);
+
+    shooterMotor.configSelectedFeedbackSensor(TalonFXFeedbackDevice.IntegratedSensor,
+        0,
+        timeout);
+
+    shooterMotor.configNominalOutputForward(0, timeout);
+    shooterMotor.configNominalOutputReverse(0, timeout);
+    shooterMotor.configPeakOutputForward(1, timeout);
+    shooterMotor.configPeakOutputReverse(-1, timeout);
+
+    shooterMotor.config_kF(0, kGains_Velocit.kF, timeout);
+    shooterMotor.config_kP(0, kGains_Velocit.kP, timeout);
+    shooterMotor.config_kI(0, kGains_Velocit.kI, timeout);
+    shooterMotor.config_kD(0, kGains_Velocit.kD, timeout);
+
   }
 
   public void setVelocity(double speed) {
-    shooterMotor.set(ControlMode.PercentOutput, 0.1);
+    shooterMotor.set(ControlMode.Velocity, 1650);
   }
 
   public void setPercentOutput(double speed) {
@@ -39,10 +54,6 @@ public class Shooter extends SubsystemBase {
 
   @Override
   public void periodic() {
-    double selSenVel = shooterMotor.getSelectedSensorVelocity(); /* position units per 100ms */
-		double vel_RotPerSec = selSenVel / 2048 * 10; /* scale per100ms to perSecond */
-    double vel_RotPerMin = vel_RotPerSec * 60.0;
-    double vel_MetersPerSec = vel_RotPerSec*2*Math.PI*0.1524;
-    SmartDashboard.putNumber("Shooter Velocity", vel_MetersPerSec);
+    SmartDashboard.putNumber("Shooter Velocity", shooterMotor.getSelectedSensorVelocity());
   }
 }
